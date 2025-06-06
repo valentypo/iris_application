88b85bcfd06a68cb4fc589e24ceea418e6c4b7e5
Gisella Jayata <gisella@Gisellas-MacBook-Pro.local>
2025-06-06 20:56:10 +0700

    benerin error

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,30 +9,30 @@ plugins {
 }
 
 android {
+    ndkVersion = "27.0.12077973"  // Keep only this NDK version declaration
     namespace = "com.example.iris_application"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
+    
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "11"
     }
-
+    
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.iris_application"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23  // Changed from flutter.minSdkVersion to 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-
+    
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
